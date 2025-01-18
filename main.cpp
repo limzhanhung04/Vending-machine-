c0c72ccedb3d8dc50f19246d62ddcb7147fa5a39
wanyazafrina <wanyazafrina@student.usm.my>
2025-01-19 03:32:35 +0800

    Update and rename customer_section.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "staff.cpp"
+
+int main() {
+
+char user;
+  
+cout << " WELCOME TO VELOR VENDING MACHINE ";
+do {
+cout << "Enter ('c') for customer and ('s') for staff: ";
+cin >> user ;
+if ( user == 's'|| user =='S')
+{
+  bool staff_login = staffLogin();
+  if (staff_login==true)
+    staffMenu();
+  else
+    continue;
+    
+}
+else if ( user == 'c' || user == 'C')
+{
+  
+}
+else
+{
+  cout << "ERROR INVALID CODE. Re-enter your code.";
+}
+}while(user != 's' || user != 'S' || user != 'c' || user != 'C')
+  }
+  return 0;  
+}
